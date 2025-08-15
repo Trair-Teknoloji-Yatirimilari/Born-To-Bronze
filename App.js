5126c9815502ebd50c0cbcb67fe418f4003cab51
Atakan Ev <atakaneliz@outlook.com>
2025-08-15 22:45:28 +0300

    Uygulama dosyalarına Sentry entegrasyonu eklendi. Hata izleme ve raporlama için gerekli ayarlar yapıldı. Ayrıca, .gitignore dosyasına .env.local eklendi ve package.json ile package-lock.json dosyalarında Sentry bağımlılığı güncellendi.

--- App.js
+++ App.js
@@ -3,12 +3,29 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://db2634ebc0eee5f102f89416bd9e9537@o4509849775505408.ingest.de.sentry.io/4509849778389072',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
       <AppNavigator />
     </GestureHandlerRootView>
   );
-}
+});
